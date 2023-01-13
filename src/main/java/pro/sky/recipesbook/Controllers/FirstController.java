a5e5264cdc6630ba5f8e3b9e5da81811dc90ee48
AndreevSV <51971450+AndreevSV@users.noreply.github.com>
2023-01-13 20:59:56 +0200

    Second commit

--- src/main/java/pro/sky/recipesbook/Controllers/FirstController.java
+++ src/main/java/pro/sky/recipesbook/Controllers/FirstController.java
@@ -1,8 +1,27 @@
 package pro.sky.recipesbook.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+
+    @GetMapping
+    public String startApp() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String studentInfo() {
+        return """
+                Имя ученика: Андреев Сергей
+                Название проекта: Книга рецептов
+                Дата создания проекта: 13.01.2023
+                Описание проекта: Книга рецептов должна стать веб-приложением, которая по названию ингридиента выдает необходимый рецепт""";
+    }
+
+
+
+
 
 }
