1fd581f920b749ec540a029a5283ada46c5ce037
AndreevSV <51971450+AndreevSV@users.noreply.github.com>
2023-03-10 13:27:40 +0200

    Second commit (HW of 23.01.2023) Здравствуй Илья! Спасибо за помощь. Функционал с ингридиентами реализовал, но столкнулся со следующей проблемой при тестировании функционала: если из файла удалить какой-то объект (не последний), то после этой операции любое добавление нового объекта (рецепта) не происходит, а просто происходит перезаписывание последнего инридиента как при методе put. Не пойму почему это происходит. Прошу еще раз помочь.

--- src/main/java/pro/sky/recipesbook/repository/IngredientRepository.java
+++ src/main/java/pro/sky/recipesbook/repository/IngredientRepository.java
@@ -1,13 +1,21 @@
 package pro.sky.recipesbook.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import pro.sky.recipesbook.model.Ingredient;
+import pro.sky.recipesbook.services.FileIngredientService;
 
 import java.util.*;
 
 @Repository
+@RequiredArgsConstructor
 public class IngredientRepository {
     private final Map<Integer, Ingredient> ingredients = new HashMap<>();
+
+    private final FileIngredientService fileIngredientService;
 
     public void addIngredient(Ingredient ingredient) {
         ingredients.put(ingredients.size() + 1, ingredient);
@@ -43,5 +51,27 @@ public class IngredientRepository {
     public Boolean deleteAllIngredients() {
         ingredients.clear();
         return true;
+    }
+
+    public void saveToFile() {
+        fileIngredientService.createFile();
+        String string = null;
+        try {
+            string = new ObjectMapper().writeValueAsString(ingredients);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        fileIngredientService.writeToFile(string);
+    }
+
+
+    public void readFromFile() {
+        try {
+            String json = fileIngredientService.readFromFile();
+            new ObjectMapper().readValue(json, new TypeReference<HashMap<Integer, Ingredient>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
